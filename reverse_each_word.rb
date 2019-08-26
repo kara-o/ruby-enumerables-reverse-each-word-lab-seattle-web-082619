a22ef9c7c6dcc715999009270c5cc699f74d932d
Kara Odle <karajo.odle@gmail.com>
2019-08-26 17:40:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,2 +1,13 @@
-reverse_each_word(string)
-  string.split(" ")
+def reverse_each_word(string)
+  
+  array = string.split(" ")
+  
+  
+  array.each do |word|
+    word.reverse!
+
+  end 
+
+  array.join(" ")
+
+end 
